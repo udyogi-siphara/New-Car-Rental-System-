123d6cbc11f0f3b2a305f2460dc8081014909822
udyogi-siphara <udyogisiphara22@gmail.com>
2025-12-25 19:18:08 +0530

    added the cars and booking pages

--- Car rental/client/src/pages/MyBookings.jsx
+++ Car rental/client/src/pages/MyBookings.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyMyBookingsData } from '../assets/assets'
+import Title from '../components/Title'
 
 const MyBookings = () => {
+
+const [bookings, setBookings] = useState([])
+
+const fetchMyBookings = async ()=> {
+  setBookings(dummyMyBookingsData)
+}
+
+useEffect(()=>{
+  fetchMyBookings()
+},[])
+
   return (
-    <div>MyBookings</div>
+    <div className='px-6 md:px-16 lg:px-24 xl:px-32 2xl:px-48 mt-16 text-sm
+    max-w-7xl'>
+          <Title title='My Bookings'
+          subTitle='View and manage your car bookings'
+          align='left'/>  
+
+
+          <div>
+            {bookings.map((booking, index)=> (
+              <div key={booking._id} className='grid grid-cols-1 md:grid-cols-4 gap-6
+              p-6 border border-borderColor rounded-lg mt-5 first:mt-12'>
+                {/* Car Image + info */}
+              </div>
+            ))}
+          </div>
+    </div>
   )
 }
 

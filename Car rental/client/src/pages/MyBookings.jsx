58dfb89b1cf813c9980080dd7e64b7bc347f906d
udyogi-siphara <udyogisiphara22@gmail.com>
2025-12-25 19:49:43 +0530

    Fully completed the booking page

--- Car rental/client/src/pages/MyBookings.jsx
+++ Car rental/client/src/pages/MyBookings.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { dummyMyBookingsData } from '../assets/assets'
+import { assets, dummyMyBookingsData } from '../assets/assets'
 import Title from '../components/Title'
 
 const MyBookings = () => {
 
 const [bookings, setBookings] = useState([])
+const currency = import.meta.env.VITE_CURRENCY
 
 const fetchMyBookings = async ()=> {
   setBookings(dummyMyBookingsData)
@@ -27,6 +28,57 @@ useEffect(()=>{
               <div key={booking._id} className='grid grid-cols-1 md:grid-cols-4 gap-6
               p-6 border border-borderColor rounded-lg mt-5 first:mt-12'>
                 {/* Car Image + info */}
+                <div className='md:col-span-1'>
+                    <div className='rounded-md overflow-hidden mb-3'>
+                      <img src={booking.car.image} alt="" className='w-full h-auto
+                      aspect-video object-cover' />
+                    </div>
+                    <p className='text-lg font-medium mt-2'>{booking.car.brand} {booking.car.model}</p>
+                    <p className='text-gray-500'>{booking.car.year} | {booking.car.category} | {booking.car.location}</p>  
+                </div>
+
+                {/* Booking info */}
+                <div className='md:col-span-2'>
+                  <div className='flex items-center gap-2'>
+                    <p className='px-3 py-1.5 bg-light rounded'>Booking #{index+1}</p>
+                    <p className={`px-3 py-1 text-xs rounded-full ${booking.status === 
+                      'confirmed' ? 'bg-green-400/15 text-green-600' : 'bg-red-400'}`}>{booking.status}</p>
+                  </div>
+
+                  <div className='flex items-start gap-2 mt-3'>
+                    <img src={assets.calendar_icon_colored} alt="" className='h-4 w-4 mt-1'/>
+                    <div>
+                      <p className='text-gray-500'>Rental Period</p>
+                      <p>{booking.pickupDate.split('T')[0]} To {booking.returnDate.split
+                      ('T')[0]}</p>
+                    </div>
+                  </div>
+
+                  <div className='flex items-start gap-2 mt-3'>
+                    <img src={assets.location_icon_colored} alt="" className='h-4 w-4 mt-1'/>
+                    <div>
+                      <p className='text-gray-500'>Pick-up Location</p>
+                      <p>{booking.car.location}</p>
+                    </div>
+                  </div>
+                  <div className='flex items-start gap-2 mt-3'>
+                    <img src={assets.location_icon_colored} alt="" className='h-4 w-4 mt-1'/>
+                    <div>
+                      <p className='text-gray-500'>Return Location</p>
+                      <p>{booking.car.location}</p>
+                    </div>
+                  </div>
+                </div>
+
+                {/* price */}
+                <div className='md:col-span-1 flex flex-col justify-between gap-6'>
+                  <div className='text-sm text-gray-500 text-right'>
+                    <p>Total Price</p>
+                    <h1 className='text-2xl font-semibold text-primary'>{currency} 
+                    {booking.price}</h1>
+                    <p>Booked on {booking.createdAt.split('T')[0]}</p>
+                  </div>
+                </div>
               </div>
             ))}
           </div>

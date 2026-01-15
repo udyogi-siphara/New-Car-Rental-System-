c6addb4908a7fdf0be204eb94c0335aa6dae285b
udyogi-siphara <udyogisiphara22@gmail.com>
2026-01-15 17:53:18 +0530

    'something'

--- Car rental/server/server.js
+++ Car rental/server/server.js
@@ -3,3 +3,13 @@ import 'dotenv/config';
 import cors from 'cors';
 
 // Intialize express App
+const app = express()
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res)=>res.send("Server is running"))
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))

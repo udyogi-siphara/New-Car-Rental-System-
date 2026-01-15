5b14f3bf8ee1bb46530c030fcfef4ac0f06649a4
udyogi-siphara <udyogisiphara22@gmail.com>
2026-01-15 20:46:35 +0530

    'Backend'

---
+++ Car rental/server/routes/userRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { registerUser,loginUser, getUserData } from "../controllers/userController.js";
+import { protect } from "../middleware/auth.js";
+
+const userRouter = express.Router();
+
+userRouter.post('/register', registerUser)
+userRouter.post('/login', loginUser)
+userRouter.get('/data', protect, getUserData)
+
+
+export default userRouter;
--- Car rental/server/server.js
+++ Car rental/server/server.js
@@ -1,15 +1,21 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import connectDB from './configs/db.js';
+import userRouter from './routes/userRoutes.js';
 
 // Intialize express App
 const app = express()
+
+// Connect Database
+await connectDB()
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res)=>res.send("Server is running"))
+app.use('/api/user', userRouter)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
